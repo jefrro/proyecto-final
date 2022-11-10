55b8e1c6f8ceb9b4ce7a2795f24f54edc3b75b0f
jefrro <jesusillo101@gmail.com>
2022-11-10 02:35:55 +0100

    init project

---
+++ AllMyGames/app/src/main/java/com/jesusfranco/proyecto/allmygames/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jesusfranco.proyecto.allmygames
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
